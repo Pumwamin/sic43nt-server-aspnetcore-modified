46f54afb0d24c629760aa9d47bf341c99fb43abc
Pumwamin <122528344+Pumwamin@users.noreply.github.com>
2024-08-22 11:31:59 +0700

    Update Index.cshtml.cs

--- SIC43NT_Webserver/Pages/Index.cshtml.cs
+++ SIC43NT_Webserver/Pages/Index.cshtml.cs
@@ -12,6 +12,7 @@ namespace SIC43NT_Webserver.Pages
     {
         public string default_key = "N/A";
         public string uid = "N/A";
+        public string nametest = "N/A";
 
         public string flagTamperTag = "-";
         public string timeStampTag_uint = "-";
@@ -28,7 +29,7 @@ namespace SIC43NT_Webserver.Pages
         public string flagTamperDecision = "N/A";
         public string rollingCodeDecision = "N/A";
 
-        public void OnGet(string d)
+        public void OnGet(string d, string t)
         {
             if (d is null)
             {
@@ -47,6 +48,10 @@ namespace SIC43NT_Webserver.Pages
                     rollingCodeServer = KeyStream.stream(default_key, timeStampTag_str, 4);
                     result_agreement_check();
                 }
+            }
+            if (t != null)
+            {
+                nametest = t;
             }
         }
         private void result_agreement_check()
